d52cbed24e5f0c0c45e11c08248e3b6c2b907dd7
Artmiss G <artmiss1380@gmail.com>
2023-10-12 03:57:31 +0330

    Basic user login authentication

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ from app.database import get_db
 import app.models as models
 
 # importing the routes
-from app.routers import songs, artists
+from app.routers import songs, artists, login
 
 app = FastAPI()
 app.include_router(songs.router) # you can also add the prefix and tags here
 app.include_router(artists.router)
-
+app.include_router(login.router)
 
 @app.get("/")
 def root(db: Session=Depends(get_db)) :
---
+++ app/routers/login.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
+
+import app.models as models
+
+from app.database import get_db
+
+router = APIRouter(
+    prefix="/login",
+    tags=["login"]
+)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@router.post("/")
+def user_login(db:Session = Depends(get_db), login_info:OAuth2PasswordRequestForm = Depends()) :
+    username, password = login_info.username, login_info.password
+
+    # check this with database
+    user = db.query(models.Subscriber).filter(
+        models.Subscriber.username == username, models.Subscriber.password == password
+    ).first()
+    if user : # if user exists or password is correct
+        return "You are logged in!"
+    else : # through exception
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid Credentials"
+        )
+
