13252e15f22e8623629a2d350b01bd2f431a8536
Artmiss G <artmiss1380@gmail.com>
2023-10-19 02:36:13 +0330

    added Artist login

--- app/routers/login.py
+++ app/routers/login.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status, Form
 from sqlalchemy.orm import Session
 
-from fastapi.security import OAuth2PasswordRequestForm 
+from fastapi.security import OAuth2PasswordRequestForm
 
 import app.models as models
 import app.auth as auth
 from app.database import get_db
 from app.utils import Password
+
+from typing import Annotated
+
 router = APIRouter(
     prefix="/login",
     tags=["login"]
@@ -14,11 +17,12 @@ router = APIRouter(
 
 
 @router.post("/")
-def login(db:Session = Depends(get_db), login_info:OAuth2PasswordRequestForm = Depends()) :
+def login(db:Session = Depends(get_db), login_info:OAuth2PasswordRequestForm = Depends(), is_artist:Annotated[bool, Form()] = False) :
     username, password = login_info.username, login_info.password
 
+    table = models.Artist_registration if is_artist else models.Subscriber
     # check this with database
-    user = db.query(models.Subscriber).filter(models.Subscriber.username == username).first()
+    user = db.query(table).filter(table.username == username).first()
     if user and  Password.validate_password(password, user.password): # if user exists and password is correct
         # creating token for the user
         data = {"username": user.username} # the data that is used
